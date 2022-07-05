6c26ef3b29a3d1a1627b6dcb88af8bdd3a893fc6
l3m4rz <l3m4rz@gmail.com>
2022-07-06 01:32:10 +0300

    refactor: изменить логику- viewer popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,110 +1,133 @@
+//* Попапы
 const editPopup = document.querySelector('#edit-popup')
 const addPopup = document.querySelector('#add-popup')
-const imgPopup = document.querySelector('.viewer-popup')
-let popup = document.querySelector('.popup');
+const viewerPopup = document.querySelector('#viewer-popup')
 
-let editButton = document.querySelector('.profile__edit-button')
+//* Поля формы редактирования
+const inputName = document.querySelector('.popup__input_el_name')
+const inputDescription = document.querySelector('.popup__input_el_description')
+//* Информация о профиле
+const profileName = document.querySelector('.profile__name')
+const profileDescription = document.querySelector('.profile__description')
 
-const editFormElement = editPopup.querySelector('.popup__form')
-editFormElement.addEventListener('submit', editFormSubmitHandler);
-
-const addFormElement = addPopup.querySelector('.popup__form')
-addPopup.addEventListener('submit', newItemFormSubmitHandler);
-
-let profileName = document.querySelector('.profile__name')
-let profileDescription = document.querySelector('.profile__description')
-let inputName = document.querySelector('.popup__input_el_name')
-let inputDescription = document.querySelector('.popup__input_el_description')
-
-function openPopup(el) {
-   el.classList.add('popup_opened');
-
-   let closeButton = el.querySelector('.popup__close-button')
-   closeButton.addEventListener('click', closePopup)
-}
-
-function closePopup(el) {
-   el.target.parentNode.parentNode.classList.remove('popup_opened')
-}
-
-function editFormSubmitHandler(evt) {
-   evt.preventDefault();
-   profileName.textContent = inputName.value
-   profileDescription.textContent = inputDescription.value
-   closePopup(evt)
-}
-
-function newItemFormSubmitHandler(evt) {
-   evt.preventDefault();
-
-   let title = addPopup.querySelector('.popup__input_el_title').value
-   let link = addPopup.querySelector('.popup__input_el_link').value
-   renderCard(title, link)
-   addPopup.querySelector('.popup__input_el_title').value = ''
-   addPopup.querySelector('.popup__input_el_link').value = ''
-
-   closePopup(evt)
-}
-
-editButton.addEventListener('click', () => {
-   inputName.value = profileName.textContent;
-   inputDescription.value = profileDescription.textContent;
-   openPopup(editPopup)
-})
-
-
+//* Кнопки открытия попапа
+const editButton = document.querySelector('.profile__edit-button')
 const addButton = document.querySelector('.profile__add-button')
-addButton.addEventListener('click', () => {
-   openPopup(addPopup)
-})
 
+//* Кнопки закрытия попапа
+const editPopupCloseButton = editPopup.querySelector('.popup__close-button')
+const addPopupCloseButton = addPopup.querySelector('.popup__close-button')
+const viewerPopupCloseButton = viewerPopup.querySelector('.popup__close-button')
 
+//* Субмиты
+const editFormElement = editPopup.querySelector('.popup__form')
+const addFormElement = addPopup.querySelector('.popup__form')
 
+//* Для рендера карточек
 const templateCard = document.querySelector('#card').content
 const cardPlace = document.querySelector('.elements')
 
+//* Поля создания новой карточки
+const newCardTitle = addPopup.querySelector('.popup__input_el_title')
+const newCardLink = addPopup.querySelector('.popup__input_el_link')
 
-const renderCard = (title, link) => {
+//* Источники во viewerPopup
+let imgTitle = viewerPopup.querySelector('.popup__viewer-title')
+let imgLink = viewerPopup.querySelector('.popup__viewer-image')
+
+//!
+
+//* добавить зарендаренную карточку
+function addCard(title, link) {
+   cardPlace.prepend(renderCard(title, link))
+}
+
+//* зарендорить карточку
+function renderCard(title, link) {
    const newCard = templateCard.querySelector('.element').cloneNode(true)
-   cardPlace.prepend(newCard)
    newCard.querySelector('.element__title').textContent = title
    newCard.querySelector('.element__image').alt = title
    newCard.querySelector('.element__image').src = link
 
    const deleteButton = newCard.querySelector('.element__delete-button')
-   deleteButton.addEventListener('click', handleDelete)
-
    const likeButton = newCard.querySelector('.element__like-button')
+   const imageButton = newCard.querySelector('.element__image')
+
+   deleteButton.addEventListener('click', handleDelete)
    likeButton.addEventListener('click', handleLike)
+   imageButton.addEventListener('click', viewerFunction)
 
-   const cardImg = newCard.querySelector('.element__image')
-
-   cardImg.addEventListener('click', openViewerPopup)
-
+   return newCard
 }
 
-function openViewerPopup(evt) {
-   const viewerPop = document.querySelector('.viewer-popup')
-
-   viewerPop.querySelector('.viewer-popup__image').src = evt.target.src
-   viewerPop.querySelector('.viewer-popup__title').textContent = evt.target.parentNode.querySelector('.element__title').textContent
-   viewerPop.querySelector('.viewer-popup__image').alt = evt.target.parentNode.querySelector('.element__title').textContent
-
-   openPopup(imgPopup)
-}
-
-const renderInitialCards = () => {
+//* добавить карточки "из коробки"
+function createInitialCards() {
    initialCards.forEach((item) => {
-      renderCard(item.name, item.link)
-   }
-   )
+      addCard(item.name, item.link)
+   })
 }
-renderInitialCards()
 
+//* открыть попап
+function openPopup(el) {
+   el.classList.add('popup_opened');
+}
+
+//* закрыть попап
+function closePopup(el) {
+   el.classList.remove('popup_opened')
+}
+
+//* Внести изменения
+function handleProfileFormSubmit(evt) {
+   evt.preventDefault();
+   profileName.textContent = inputName.value
+   profileDescription.textContent = inputDescription.value
+   closePopup(editPopup)
+}
+
+//*  Добавить новую карточку пользователя
+function handleAddFormSubmit(evt) {
+   evt.preventDefault();
+   addCard(newCardTitle.value, newCardLink.value)
+   closePopup(addPopup)
+   newCardTitle.value = ''
+   newCardLink.value = ''
+}
+
+//* удалить карточку
+function handleDelete(evt) {
+   evt.target.closest('.element').remove()
+}
+
+//* like/dislike
 function handleLike(evt) {
    evt.target.classList.toggle('element__like-button_active')
 }
 
-function handleDelete(evt) {
-   evt.target.closest('.element').remove()
+//* Открыть нужную карточку во viewer
+function viewerFunction(evt) {
+   imgLink.src = evt.target.src
+   imgTitle.textContent = evt.target.alt
+   openPopup(viewerPopup)
 }
+
+//!
+
+//* Слушатели для редактирования профиля
+editButton.addEventListener('click', () => {
+   inputName.value = profileName.textContent;
+   inputDescription.value = profileDescription.textContent;
+   openPopup(editPopup)
+});
+editPopupCloseButton.addEventListener('click', () => closePopup(editPopup));
+editFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+//* Слушатели для добавления карточки
+addButton.addEventListener('click', () => { openPopup(addPopup) });
+addPopupCloseButton.addEventListener('click', () => closePopup(addPopup));
+addFormElement.addEventListener('submit', handleAddFormSubmit);
+
+//* Слушатели для просмотра
+viewerPopupCloseButton.addEventListener('click', () => closePopup(viewerPopup))
+
+createInitialCards()
